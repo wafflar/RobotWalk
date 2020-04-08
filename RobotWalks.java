e8d8077c2355072b88bbab5dfbb5eefc3bc5a2f4
keet <wwukuww@gmail.com>
2020-04-08 12:22:30 +0900

    Initial sharing of project

---
+++ RobotWalks.java
@@ -0,0 +1,33 @@
+
+/**
+ * クラス RobotWalks の注釈をここに書きます.
+ * 
+ * @author (あなたの名前)
+ * @version (バージョン番号もしくは日付)
+ */
+public class RobotWalks
+{
+    // インスタンス変数 - コードに合わせて説明を書き換えます.
+    private int x;
+
+    /**
+     * RobotWalks クラスのインスタンスのためのコンストラクタ
+     */
+    public RobotWalks()
+    {
+        // インスタンス変数の初期化
+        x = 0;
+    }
+
+    /**
+     * メソッドの例 - メソッドとともにこのコメントも置き換えてください.
+     * 
+     * @param  y    メソッドのためのサンプル引数
+     * @return        x と y の和
+     */
+    public int sampleMethod(int y)
+    {
+        // ここにあなたのコードを書きます
+        return x + y;
+    }
+}
